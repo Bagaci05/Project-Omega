03c0f67e57e485d404667de8ad699875006e8756
Róland <rusznyakroland111@gmail.com>
2025-11-08 14:43:13 +0100

    hoppá

--- configScripts/switches/S-K1.sh
+++ configScripts/switches/S-K1.sh
@@ -20,25 +20,25 @@ line con 0
 logging synchronous
 exit
 
-ip dhcp snooping
-ip dhcp snooping vlan 420,888
-ip arp inspection vlan 420,888
+# ip dhcp snooping
+# ip dhcp snooping vlan 420,888
+# ip arp inspection vlan 420,888
 
 int range g0/1-2
 #ip verify source
 switchport mode access
 switchport access vlan 420
-switchport port-security
-switchport port-security maximum 2
-switchport port-security mac-address sticky
-switchport port-security aging time 10
-spanning-tree portfast
-spanning-tree bpduguard enable
+# switchport port-security
+# switchport port-security maximum 2
+# switchport port-security mac-address sticky
+# switchport port-security aging time 10
+# spanning-tree portfast
+# spanning-tree bpduguard enable
 exit
 
-int g0/1
-ip dhcp snooping trust
-ip arp inspection trust
+# int g0/1
+# ip dhcp snooping trust
+# ip arp inspection trust
 
 int g0/0
 switchport trunk encapsulation dot1q
@@ -46,8 +46,8 @@ switchport mode trunk
 switchport trunk allowed vlan 420,888
 switchport trunk native vlan 888
 switchport nonegotiate
-ip dhcp snooping trust
-ip arp inspection trust
+# ip dhcp snooping trust
+# ip arp inspection trust
 exit
 
 int range g0/3,g1/0-3,g2/0-3,g3/0-3
--- configScripts/switches/S-K5.sh
+++ configScripts/switches/S-K5.sh
@@ -16,13 +16,13 @@ vlan 888
 vlan 369
 exit
 
-ip dhcp snooping
-ip dhcp snooping vlan 51,888
-ip arp inspection vlan 51,888
-
 line con 0
 logging synchronous
 exit
+
+# ip dhcp snooping
+# ip dhcp snooping vlan 51,888
+# ip arp inspection vlan 51,888
 
 int range g2/0-3
 switchport trunk encapsulation dot1q
@@ -30,21 +30,21 @@ switchport mode trunk
 switchport trunk allowed vlan 51,888
 switchport trunk native vlan 888
 switchport nonegotiate
-ip dhcp snooping trust
-ip arp inspection trust
 channel-group 2 mode auto
+# ip dhcp snooping trust
+# ip arp inspection trust
 exit
 
 int g0/0
 #ip verify source
 switchport mode access
 switchport access vlan 51
-switchport port-security
-switchport port-security maximum 2
-switchport port-security mac-address sticky
-switchport port-security aging time 10
-spanning-tree portfast
-spanning-tree bpduguard enable
+# switchport port-security
+# switchport port-security maximum 2
+# switchport port-security mac-address sticky
+# switchport port-security aging time 10
+# spanning-tree portfast
+# spanning-tree bpduguard enable
 exit
 
 int range g0/1-3,g1/0-3,g3/0-3
