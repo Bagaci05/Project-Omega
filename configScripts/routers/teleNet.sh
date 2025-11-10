cdd0130e8598ff2e71c2b1b56601034d38a39ac6
bagaci05 <blaga.mate05@gmail.com>
2025-11-10 12:09:46 +0100

    yessir

--- configScripts/routers/teleNet.sh
+++ configScripts/routers/teleNet.sh
@@ -17,6 +17,10 @@ exit
 
 access-list 1 permit any
 ip nat inside source list 1 int g6/0 overload
+ip nat inside source static tcp 10.0.0.250 10050 192.168.1.153 10050
+ip nat inside source static tcp 10.0.0.250 10051 192.168.1.153 10051
+ip nat inside source static tcp 10.0.0.250 80 192.168.1.153 10049
+ip nat inside source static tcp 10.0.0.250 443 192.168.1.153 10048
 
 ip route 0.0.0.0 0.0.0.0 192.168.1.1
 router ospf 1
