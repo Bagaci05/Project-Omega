fa8acc06c8ff408a6384131864a320e30923ed34
bagaci05 <blaga.mate05@gmail.com>
2026-01-25 16:41:34 +0100

    noZabbixx

--- configScripts/routers/teleNet.sh
+++ configScripts/routers/teleNet.sh
@@ -22,10 +22,6 @@ exit
 
 access-list 1 permit any
 ip nat inside source list 1 int g6/0 overload
-ip nat inside source static tcp 10.0.0.250 10050 192.168.1.153 10050
-ip nat inside source static tcp 10.0.0.250 10051 192.168.1.153 10051
-ip nat inside source static tcp 10.0.0.250 80 192.168.1.153 10049
-ip nat inside source static tcp 10.0.0.250 443 192.168.1.153 10048
 ip nat inside source static tcp 10.1.0.253 80 192.168.1.153 8080
 
 ip route 0.0.0.0 0.0.0.0 192.168.1.1
