3f327fb3e9db515c2d8e5c1c99476897976862f0
bagaci05 <blaga.mate05@gmail.com>
2026-01-31 00:16:24 +0100

    updateToWork

--- configScripts/Windows/UserAdd.ps1
+++ configScripts/Windows/UserAdd.ps1
@@ -1,29 +1,29 @@
 Import-Module ActiveDirectory
-
+ 
 Write-Host "=== New AD User Creation ===" -ForegroundColor Cyan
-
+ 
 # Ask basic info
 $FirstName = Read-Host "First name"
 $LastName  = Read-Host "Last name"
 $Username  = Read-Host "Username (samAccountName)"
 $Password  = Read-Host "Password" -AsSecureString
-
+ 
 # Ask user type
 Write-Host ""
 Write-Host "Select user type:" -ForegroundColor Yellow
 Write-Host "1 - Guest"
 Write-Host "2 - Worker"
-
+ 
 $TypeChoice = Read-Host "Enter 1 or 2"
-
+ 
 switch ($TypeChoice) {
     "1" {
-        $OUPath = "OU=Guests,OU=evils,DC=EvilCaffee.hu,DC=local"
+        $OUPath = "OU=Guests,OU=evils,DC=Evilcaffee,DC=hu"
         $RoleGroup = "EvilGuests"
         $UserType = "Guest"
     }
     "2" {
-        $OUPath = "OU=workers,OU=evils,DC=EvilCaffee.hu,DC=local"
+        $OUPath = "OU=Workers,OU=evils,DC=Evilcaffee,DC=hu"
         $RoleGroup = "EvilWorkers"
         $UserType = "Worker"
     }
@@ -32,11 +32,11 @@ switch ($TypeChoice) {
         exit 1
     }
 }
-
+ 
 # Home folder
 $HomeDrive = "H:"
 $HomePath  = "\\CaffeeServer\home\$Username"
-
+ 
 # Create user
 New-ADUser `
     -Name "$FirstName $LastName" `
@@ -49,11 +49,11 @@ New-ADUser `
     -Path $OUPath `
     -HomeDrive $HomeDrive `
     -HomeDirectory $HomePath
-
+ 
 # Add to security groups
-Add-ADGroupMember -Identity "WIFI Users" -Members $Username
+Add-ADGroupMember -Identity "WIFIUsers" -Members $Username
 Add-ADGroupMember -Identity $RoleGroup -Members $Username
-
+ 
 Write-Host ""
 Write-Host "User created successfully!" -ForegroundColor Green
 Write-Host "Name: $FirstName $LastName"
