26191dc24577580728b2c94a92af1ae1d0c4ab57
Blága Máté <blaga.mate05@gmail.com>
2026-03-04 09:04:17 +0100

    Cafffee - Layer 2 Security

--- 2.Caffee/caffeeswitch.sh
+++ 2.Caffee/caffeeswitch.sh
@@ -15,19 +15,39 @@ int g0/0
 switchport trun encapsulation dot1q
 switchport mode trun
 switchport trunk allowed vlan 10,20,30
+ip dhcp snooping trust
 exit
 
 int range g0/1-3
 switchport mode access
 switchport access vlan 20
+switchport port-security maximum 1
+switchport port-security violation shutdown
+switchport port-security mac-address sticky
+spanning-tree portfast
+spanning-tree bpduguard enable
 exit
 
 int range g2/0-2
 switchport mode access
 switchport access vlan 10
+switchport port-security maximum 1
+switchport port-security violation shutdown
+switchport port-security mac-address sticky
+spanning-tree portfast
+spanning-tree bpduguard enable
 exit
 
 int g3/3
 switchport mode access
 switchport access vlan 30
+switchport port-security maximum 1
+switchport port-security violation shutdown
+switchport port-security mac-address sticky
+spanning-tree portfast
+spanning-tree bpduguard enable
+exit
+
+int g0/2
+ip dhcp snooping trust
 exit
